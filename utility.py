6dbd984717faeefbf0ecbca8474362ac96969baa
Dmitry Ivakhnenko <dmitry.ivakhnenko@gmail.com>
2019-12-02 21:48:03 +0300

    Add utility.py

---
+++ utility.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Набор вспомогательных функций.
+
+Version: 0.1
+Created: 02/12/2019
+Last modified: 02/12/2019
+"""
+
+import requests
+import time
+from bs4 import BeautifulSoup as bs
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    __time_start = time.perf_counter()
+    main()
+    __time_delta = time.perf_counter() - __time_start
+    __TIMES = (('d', 24 * 60 * 60), ('h', 60 * 60), ('m', 60), ('s', 1))
+    __times = ''
+    for __i in range(len(__TIMES) - 1):
+        __t, __time_delta = divmod(__time_delta, __TIMES[__i][1])
+        if __t > 0:
+            __times += "{} {} ".format(int(__t), __TIMES[__i][0])
+    __times += "{:.3} {}".format(__time_delta, __TIMES[~0][0])
+    print("\n[Finished in {}]".format(__times))
